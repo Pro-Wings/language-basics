e3edf6f68d56741d42bd40de5feb46922f6380e0
JD <jayprowings@gmail.com>
2023-03-11 11:08:09 +0530

    overloading scenario

--- src/com/prowings/polymorphism/overloading/MainMethodOverloadingDemo.java
+++ src/com/prowings/polymorphism/overloading/MainMethodOverloadingDemo.java
@@ -3,33 +3,6 @@ package com.prowings.polymorphism.overloading;
 public class MainMethodOverloadingDemo {
 	
 	
-	int roll;
-	String name;
-	
-	public MainMethodOverloadingDemo()
-	{
-		
-	}
-	public MainMethodOverloadingDemo(int roll)
-	{
-		this.roll = roll;
-	}
-	public MainMethodOverloadingDemo(String name)
-	{
-		this.name = name;
-	}
-	public MainMethodOverloadingDemo(int roll, String name)
-	{
-		this.name = name;
-		this.roll = roll;
-	}
-	public MainMethodOverloadingDemo(String name, int roll)
-	{
-		this.name = name;
-		this.roll = roll;
-	}
-	
-	
 	
 	 static public void main(String...args) {
 		System.out.println("inside original main(String[]) method");
--- src/com/prowings/polymorphism/overloading/OverloadingDemo7.java
+++ src/com/prowings/polymorphism/overloading/OverloadingDemo7.java
@@ -6,6 +6,10 @@ public class OverloadingDemo7 {
 		System.out.println("int array");
 	}
 
+//	public static void test(int... intArr) {
+//		System.out.println("int array");
+//	}
+
 	public static void test(long[] longArr) {
 		System.out.println("long array");
 	}
@@ -14,6 +18,11 @@ public class OverloadingDemo7 {
 		System.out.println("char array");
 	}
 
+	public static void test(Object o) {
+		System.out.println("Object o");
+	}
+
+	
 	public static void main(String[] args) {
 		int[] nums = { 1, 2, 3, 4, 5 };
 		test(nums);
