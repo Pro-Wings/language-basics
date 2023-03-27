87ea9549bf6e46ec4623a52986c081711cc26fa5
JD <jayprowings@gmail.com>
2023-03-27 09:38:21 +0530

    recursive method

---
+++ src/com/prowings/recursion/TestRecursion.java
@@ -0,0 +1,22 @@
+package com.prowings.recursion;
+
+public class TestRecursion {
+
+	public static int factorial(int n) {
+
+		if (n != 0) // termination condition
+			return n * factorial(n - 1); // recursive call
+		else
+			return 1;
+	}
+
+	
+	
+	public static void main(String[] args) {
+		
+		int res = factorial(5);
+		
+		System.out.println(res);
+		
+	}
+}
